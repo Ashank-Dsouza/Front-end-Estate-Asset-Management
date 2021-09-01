ab3a4ea1f589b0f8106700116c7905f678315c91
Ashank-Dsouza <ashankdsouza054@yahoo.com>
2021-09-01 13:11:55 +0530

    removes un needed junk

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,39 +1,21 @@
 import React from 'react';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { Link } from 'react-router-dom';
 
-import {
-    Typography, Container, CssBaseline, IconButton, List,
-    ListItem, ListItemText, ListItemIcon, Button, AppBar, Toolbar, Hidden, Menu, MenuItem, Drawer, Divider, CardMedia, Grid
+import { Button, AppBar, Toolbar, 
 } from "@material-ui/core";
-import AddUserForm from "../components/forms/AddUserForm";
 import { PostWithAuth } from '../apis/api-controller';
 import { GetDeviceId } from '../utility/ApiHelperFunctions';
 import PropTypes from 'prop-types'
 
 
 import { green } from "@material-ui/core/colors";
-import DehazeIcon from '@material-ui/icons/Dehaze';
-import AppsIcon from '@material-ui/icons/Apps';
-import AndroidIcon from '@material-ui/icons/Android';
 import { withRouter } from 'react-router';
 
-import { useState, useEffect } from 'react';
 
-
-const options = [
-    'Subscriber',
-    'User',
-];
 
 const ITEM_HEIGHT = 48;
 
 function NavBar(props) {
-
-    const [drawerOpen, setDrawerOpen] = React.useState(false);
-    const [menuOpen, setMenuOpen] = React.useState(false);
-    const [gender, setGender] = React.useState('male');
-    const selectGender = (event) => setGender(event.target.value);
 
     async function Logout() {
         const device_id = await GetDeviceId();
