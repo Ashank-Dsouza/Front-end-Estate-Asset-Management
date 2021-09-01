442d458a9463a5d11311698cdbb7b7c693b1ce10
Ashank-Dsouza <ashankdsouza054@yahoo.com>
2021-09-01 12:57:40 +0530

    refactors map users page

--- src/pages/MapUser.js
+++ src/pages/MapUser.js
@@ -4,60 +4,81 @@ import { GetWithAuth, PostWithAuth } from "../apis/api-controller";
 
 import React from "react";
 
-import {    Container, CssBaseline, Paper,  Table,
-            TableContainer, TableBody, TableHead,
-            TableRow, TableCell, TablePagination,
-            MenuItem, Link, Select}
-from "@material-ui/core";
+import {
+    Container, CssBaseline, Paper, Table,
+    TableContainer, TableBody, TableHead,
+    TableRow, TableCell, TablePagination,
+    MenuItem, Link, Select, CircularProgress
+}
+    from "@material-ui/core";
 import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types'
+import { withRouter } from "react-router-dom";
 
 
+import NotEnoughPermissions from "./NotEnoughPermissions";
 import NavBar from "../components/NavBar";
 
-export default function MapUser() {
+function MapUser(props) {
 
     const [page, setPage] = useState(0);
     const [row, setRow] = useState(10);
 
     var selectedUsers = [];
 
-    var [userList, setUsers] = useState([{ 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }, { 'username': 'Ahmad.Nader89', 'email': "Ahmad_Nader91@yahoo.com", "Gender": 'Female', "City": "South Coleville", "Status": 'Approved' }])
-
+    var [userList, setUsers] = useState(null);
     useEffect(() => {
         GetUsersAndDisplay();
-
     }, []);
 
     async function GetUsersAndDisplay() {
         GetWithAuth('/users')
             .then((response) => {
+                console.log("the user data is: ", response);
                 const UserDataList = FormatData(response.data)
                 setUsers(UserDataList);
             })
+            .catch((error) => {
+                props.history.push('/not-allowed');
+            })
+    }
+
+    function GetAssignableRoles(roles) {
+        if(!roles)
+            return "Unassigned";
+        for (let index = 0; index < roles.length; index++) {
+            const role = roles[index];
+            if(role.name === "Guest" || role.name === "Customer"){
+                return role.name
+            }
+        } 
+            return "Unassigned";
     }
 
     function FormatData(UserDataList) {
-        if(!UserDataList || UserDataList.length == 0){
+        if (!UserDataList || UserDataList.length == 0) {
             return [];
         }
         var formattedUserData = [];
         for (let index = 0; index < UserDataList.length; index++) {
             const User = UserDataList[index];
+            const role = GetAssignableRoles(User.roles)
 
             formattedUserData.push({
                 'username': User.username,
                 'email': User.email,
                 'id': User.id,
+                'role': role
             })
         }
         return formattedUserData;
     }
 
     function ChangeSelection(UserId, Selected) {
-        if(Selected){
+        if (Selected) {
             AddUserToSelection(UserId);
         }
-        else{
+        else {
             DeleteUserToSelection(UserId);
         }
     }
@@ -66,63 +87,91 @@ export default function MapUser() {
         selectedUsers.push(UserId);
     }
     function DeleteUserToSelection(UserId) {
-        selectedUsers = selectedUsers.filter(function(item) {
+        selectedUsers = selectedUsers.filter(function (item) {
             return item !== UserId
         })
     }
 
-  const handleChange = (event) => {
-    const chosenRole = event.target.value;
-    const roleToRoleId ={
-        "Guest": "4",
-        "Customer": "3"
-    }
-    console.log(roleToRoleId[chosenRole]);
-    const roleId = roleToRoleId[chosenRole];
+    const handleChange = (event) => {
+        if(selectedUsers.length === 0){
+            console.log("no user selected. ");
+            return
+        }
+        const chosenRole = event.target.value;
+        const roleToRoleId = {
+            "Guest": "4",
+            "Customer": "3"
+        }
+        console.log(roleToRoleId[chosenRole]);
+        const roleId = roleToRoleId[chosenRole];
 
-    const url = "/roles/" + roleId + "/users";
-    PostWithAuth(url, {
-        "users": selectedUsers
-    }).then((response) =>{
-        console.log("assigned role to users");
-    })
-  };
+        const url = "/roles/" + roleId + "/users";
+        PostWithAuth(url, {
+            "users": selectedUsers
+        }).then((response) => {
+            GetUsersAndDisplay();
+            console.log("assigned role to users");
+
+        })
+    };
 
     return (
-        <>
-            <CssBaseline />
-            <NavBar/>
-      
-            <Container style={{ marginTop: 10 }}>
-            <Select onChange={handleChange}
-                        label="Role" variant='outlined' fullWidth>
-                        <MenuItem value={'Guest'} >Guest</MenuItem>
-                        <MenuItem value={'Customer'}>Customer</MenuItem>
-                    </Select>
-                <TableContainer component={Paper}>
-                    <Link href="/add-user" className="btn btn-primary">Add User</Link>
 
-                    {/* <Button variant='contained' color='primary'>Add User</Button> */}
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>User Name</TableCell>
-                                <TableCell>Email</TableCell>
-                                <TableCell>Gender</TableCell>
-                                <TableCell>City</TableCell>
-                                <TableCell>Status</TableCell>
-                                <TableCell>Role</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {userList.slice(page * row, (page + 1) * row).map((item) => (
-                                <UserRow UpdateUsers={GetUsersAndDisplay} UserData={item} onCheckBoxChange={ChangeSelection} />
-                            ))}
-                            <TablePagination rowsPerPageOptions={[2, 4, 10, 15]} count={userList.length} rowsPerPage={row} page={page} onChangePage={(event, newPage) => setPage(newPage)} onChangeRowsPerPage={(event) => setRow(event.target.value)} />
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </Container>
+        <>
+            <NavBar />
+
+            {
+                userList === null ?
+                    (
+                        <CircularProgress />
+                    ) :
+                    (
+                        <div>
+                            <CssBaseline />
+                            
+                            <Container style={{ marginTop: 10 }}>
+                            <Link href="/add-user" className="btn btn-primary">Add User</Link>
+
+                            <Select  onChange={handleChange}
+                                    label="Role" variant='outlined'  style={{ marginTop: 10,
+                                                                                       float:"right"
+                                                                                     }}>
+                                    <MenuItem value={'Guest'} >Guest</MenuItem>
+                                    <MenuItem value={'Customer'}>Customer</MenuItem>
+                                </Select>
+                                
+                                <TableContainer component={Paper}>
+
+                                    <Table>
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell>User Name</TableCell>
+                                                <TableCell>Email</TableCell>
+                                                <TableCell>Gender</TableCell>
+                                                <TableCell>City</TableCell>
+                                                <TableCell>Status</TableCell>
+                                                <TableCell>Role</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {userList.slice(page * row, (page + 1) * row).map((item) => (
+                                                <UserRow UpdateUsers={GetUsersAndDisplay} UserData={item} onCheckBoxChange={ChangeSelection} />
+                                            ))}
+                                            <TablePagination rowsPerPageOptions={[2, 4, 10, 15]} count={userList.length} rowsPerPage={row} page={page} onChangePage={(event, newPage) => setPage(newPage)} onChangeRowsPerPage={(event) => setRow(event.target.value)} />
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </Container>
+                        </div>
+                    )
+
+            }
         </>
     );
 }
+
+export default withRouter(MapUser);
+
+MapUser.propTypes = {
+    history: PropTypes.object.isRequired,
+};
