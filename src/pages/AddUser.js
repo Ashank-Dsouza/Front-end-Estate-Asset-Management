a49c1ab5578d01acdb3cf3cd7e7fc31f65083575
Ashank-Dsouza <ashankdsouza054@yahoo.com>
2021-09-01 12:56:45 +0530

    adds alert when user has been added

--- src/pages/AddUser.js
+++ src/pages/AddUser.js
@@ -16,6 +16,10 @@ import AndroidIcon from '@material-ui/icons/Android';
 
 export default function AddUser(props) {
 
+    function SuccessfullyAddedUser(params) {
+        alert('added user!');
+    }
+
 
     return (
 
@@ -23,13 +27,10 @@ export default function AddUser(props) {
             <CssBaseline />
             <NavBar/>
             <Container style={{ marginTop: 10, marginBottom: 32 }}>
-                <Typography variant='h4' style={{ color: green[700], marginBottom: 20 }}>Personal Information</Typography>
+                <Typography variant='h4' style={{ color: green[700], marginBottom: 20 }}>Add User</Typography>
                 <Grid container justifyContent='space-between'>
-                    <Grid item md={5} sm={12} xs={12}>
-                        <CardMedia src="https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg" component="img" style={{ width: 400, maxWidth: '100%' }} />
-                    </Grid>
                     <Grid item md={6} sm={12} xs={12} style={{ paddingBottom: 10 }}>
-                        <AddUserForm ShowAgreementCheckbox={false}/>
+                        <AddUserForm AddedUserAlert={SuccessfullyAddedUser} ShowAgreementCheckbox={false}/>
                     </Grid>
                 </Grid>
             </Container>
