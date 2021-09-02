161517ec165813e3b372e0c5ebd47a770dc99f59
Ashank-Dsouza <ashankdsouza054@yahoo.com>
2021-09-02 09:30:31 +0530

    places setting page inside a container

--- src/pages/AccountSetting.js
+++ src/pages/AccountSetting.js
@@ -2,14 +2,20 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import NavBar from "../components/NavBar";
 import { RoutePath } from "../constants/routes";
+import {
+    Container
+}
+    from "@material-ui/core";
 
 export default function AccountSettingPage(props) {
 
     return (
         <>
             <NavBar/>
+            <Container style={{ marginTop: 20 }}>
            <h1>Account Settings</h1>
            <Link to={RoutePath.PasswordResetPage}>Change Password</Link>
+           </Container>
 
         </>
     );
