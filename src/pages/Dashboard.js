beece754506c3fa1bd3eca848902a97e2ae4e7d4
Ashank-Dsouza <ashankdsouza054@yahoo.com>
2021-09-06 10:22:06 +0530

    removes delete user, fixes dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,13 +2,20 @@ import { Typography } from "@material-ui/core";
 import PropTypes from 'prop-types'
 import Heading from "../components/Heading";
 import NavBar from "../components/NavBar";
+import {
+    Container
+}
+    from "@material-ui/core";   
 
 function Dashboard(props) {
  
     return (
         <>
             <NavBar></NavBar>
+            <Container style={{ marginTop: 20 }}>
+
             <Heading Text={"Dashboard"}></Heading>
+            </Container>
         </>
     )
 
